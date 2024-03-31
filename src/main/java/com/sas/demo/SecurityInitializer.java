cccad77c45420c065a4d37ed21faa0e9bca0b3b7
howToHitA7iron <163889941+howToHitA7iron@users.noreply.github.com>
2024-03-31 10:35:59 -0400

    Add files via upload

---
+++ src/main/java/com/sas/demo/SecurityInitializer.java
@@ -0,0 +1,9 @@
+package com.sas.demo;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+// This class will help to register spring security 
+// filter chain with our application 
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer { 
+  
+}
